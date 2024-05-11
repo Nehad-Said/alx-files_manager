22a0ac80a3c32a949f25be75e82df698ee602678
bennet489 <bennetasare@outlook.com>
2024-05-11 12:11:19 +0300

    Rectify errors in index.js

--- routes/index.js
+++ routes/index.js
@@ -18,5 +18,20 @@ const injectRoutes = (api_ => {
     api_.get('/connect', basicAuthenticate, AuthController.getConnect);
     api.get('/disconnect', xTokenAuthenticate, AuthController.getDisconnect);
 
-    api.post('/users', UsersControo)
-})
+    api.post('/users', UsersControl.postNew);
+    api.get('/users/me', xTokenAuthenticate, UsersController.getMe);
+
+    api.post('/files', xTokenAuthenticate, FilesController.postUpload);
+    api.get('/files/:id', xTokenAuthenticate, FilesController.getShow);
+    api.get('files', xTokenAuthenticate, FilesController.getIndex);
+    api.put('/files/:id/publish', xTokenAuthenticate, FilesController.putPublish);
+    api.put('/files/:id/unpublish', xTokenAuthenticate, FilesController.putUnpublish);
+    api.get('/files/:id/data', FilesController.getFile);
+
+    api.all('*', (req, res, next) => {
+        errorResponse(new APIError(404, `Cannot ${req.method} ${req.url}`, req, res, next ));
+    });
+    api.use(errorResponse);
+});
+
+export default injectRoutes;
