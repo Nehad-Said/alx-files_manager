3dcdbf7b144350ec9651ff09a0db99bc7df47b15
bennet489 <bennetasare@outlook.com>
2024-05-14 13:08:02 +0300

    Implementation of middlewares to the express application in middlewares.js

---
+++ libs/middlewares.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+/**
+ * Implementing middleware to this express application
+ * @param {express.Express} api this is express application
+ */
+const injectMiddlewares = (api) => {
+    api.use(express.json({ limit: '200mb' }));
+};
+
+export default injectMiddlewares;
